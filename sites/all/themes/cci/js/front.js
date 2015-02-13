a6ece9e6a651818e7f461d60e5b45d3d4e997393
Chris Morris <cmorris@ccistudios.com>
2015-02-13 11:43:56 -0500

    Style some of the home blocks

--- sites/all/themes/cci/js/front.js
+++ sites/all/themes/cci/js/front.js
@@ -1,5 +1,15 @@
 (function($) {
     $(function(){        
         $(".block.home-header").height($(window).height());
+        
+        $(".block.home-header .content a, .home-arrow-down")
+        .click(function(){
+            var animationTime = parseInt(1000 * (($(window).height() - $("body").scrollTop()) / $(window).height()));
+            $("html, body").animate({
+                "scrollTop": $(window).height() - 51 + "px"
+            }, animationTime);
+            
+            return false;
+        });
     });
 }(jQuery));
