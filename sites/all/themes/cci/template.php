e9d89fb54f89c5042b12ee9b7aaadc9dde4a0247
Chris Morris <cmorris@ccistudios.com>
2015-02-20 10:10:59 -0500

    Style the start a project form

--- sites/all/themes/cci/template.php
+++ sites/all/themes/cci/template.php
@@ -14,4 +14,12 @@ function cci_preprocess_page(&$variables, $hook)
     drupal_add_library('system','effects');
 }
 
+function cci_form_alter(&$form, &$form_state, $form_id)
+{
+    if ($form_id == 'webform_client_form_112')
+    {
+        $form['submitted']['type_of_project']['#empty_option'] = 'Unknown/Other';
+    }
+}
+
 ?>
