67d019eba8f81a4aa77db0b05da87bd3db37c6e3
Chris Morris <cmorris@ccistudios.com>
2015-02-24 14:07:58 -0500

    Added drush alias for live

--- sites/all/drush/aliases.drushrc.php
+++ sites/all/drush/aliases.drushrc.php
@@ -11,3 +11,13 @@ $aliases['dev'] = array(
 	'ssh-options'=>'-p 37241'
 );
 
+$aliases['live'] = array(
+	'uri'=> 'live.ccistudios.com',
+	'root' => '/home/ccistud/subdomains/live/public_html',
+	'remote-host'=> 'host.ccistudios.com',
+	'remote-user'=> 'ccistud',
+	'path-aliases'=> array(
+		'%files'=> 'sites/default/files',
+	),
+	'ssh-options'=>'-p 37241'
+);
