3f32a097e5905715c81a2ea7789c945e204f2c55
Chris Morris <cmorris@ccistudios.com>
2015-11-03 11:33:50 -0500

    added toronto office to the contact area and map

--- sites/all/themes/cci/js/map.js
+++ sites/all/themes/cci/js/map.js
@@ -8,32 +8,45 @@
 
     function createMap()
     {
-        var location = new google.maps.LatLng(42.971760, -82.40737);
         var mapOptions = {
-            zoom: 17,
-            center: location,
+            zoom: getDefaultZoom(),
+            center: new google.maps.LatLng(43.401992, -80.886841),
             scrollwheel: false
         }
         var map = new google.maps.Map($(".gmap").get(0),
                         mapOptions);
 
         var image = '/sites/all/themes/cci/images/maps-marker.svg';
-        var marker = new google.maps.Marker({
-            position: location,
+        var sarnia = new google.maps.Marker({
+            position: new google.maps.LatLng(42.971760, -82.40737),
+            map: map,
+            icon: image,
+            title: "CCI Studios"
+        });
+        var toronto = new google.maps.Marker({
+            position: new google.maps.LatLng(43.6434672,-79.39400),
             map: map,
             icon: image,
             title: "CCI Studios"
         });
         
-        
-        var infowindow = new google.maps.InfoWindow({
-            content: "<h2>This is where the Good Times happen</h2>",
-            maxWidth: 200
+        google.maps.event.addListener(sarnia, 'click', function() {
+            map.setCenter(sarnia.getPosition());
+            map.setZoom(15);
         });
-        google.maps.event.addListener(marker, 'click', function() {
-            infowindow.open(map, marker);
-            $(".gmap [style*='skewX']").css("background","#106232");
-            $(".gmap").find('div[style*="rgb(255, 255, 255)"]').filter(function () { return this.innerHTML == "" }).css("background","#106232");
+        google.maps.event.addListener(toronto, 'click', function() {
+            map.setCenter(toronto.getPosition());
+            map.setZoom(15);
         });
+    }
+    function getDefaultZoom() {
+        var w = $(window).width();
+        if (w > 760) {
+            return 8;
+        }
+        if (w > 480) {
+            return 7;
+        }
+        return 6;
     }
 }(jQuery));
