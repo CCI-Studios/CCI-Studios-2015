8dd7b96eedfee8036874ac3a69357803591acab9
Chris Morris <cmorris@ccistudios.com>
2019-01-17 09:31:43 -0500

    fix twitter feed

--- sites/all/themes/cci/js/twitter.js
+++ sites/all/themes/cci/js/twitter.js
@@ -1,19 +1,31 @@
 (function($) {
     $(function(){
         var config = {
-          "id": '565945598277214208',
-          "maxTweets": 3
-        };
-        twitterFetcher.fetch(config, callback);
+            "profile": {"screenName": 'ccistudios'},
+            "maxTweets": 3,
+            "enableLinks": true, 
+            "showUser": true,
+            "showTime": false,
+            "showImages": true,
+            "lang": "en",
+            "dataOnly": true,
+            "customCallback": callback
+          };
+        twitterFetcher.fetch(config);
     });
     
-    function callback(tweets)
-    {
+    function callback(tweets) {
         $(".block.twitter .feed").empty();
         for (var i in tweets)
         {
             var t = tweets[i];
-            $(".block.twitter .feed").append("<div class='tweet'><div class='text'><a href='"+t.link+"' target='_blank'>"+t.text+"</a></div><div class='avatar'><a href='"+t.userLink+"' target='_blank'><img src='"+t.avatar+"' alt='"+t.name+"' /></a></div><div class='handle'><a href='"+t.userLink+"' target='_blank'>"+t.handle+"</a></div></div>");
+            var link = t.permalinkURL;
+            var text = t.tweet;
+            var userLink = t.author_data.profile_url;
+            var avatar = t.author_data.profile_image;
+            var name = t.author_data.name;
+            var handle = t.author_data.screen_name;
+            $(".block.twitter .feed").append("<div class='tweet'><div class='text'><a href='"+link+"' target='_blank'>"+text+"</a></div><div class='avatar'><a href='"+userLink+"' target='_blank'><img src='"+avatar+"' alt='"+name+"' /></a></div><div class='handle'><a href='"+userLink+"' target='_blank'>"+handle+"</a></div></div>");
         }
     }
 }(jQuery));
